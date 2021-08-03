67fd4638bdbfacbf0c92cf424057a40058a85ac3
tatialveso <tatiialveso@gmail.com>
2021-08-03 13:44:32 -0300

    :sparkles: create home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: RegisterComponent },
   { path: 'contato', component: ContactComponent },
+  { path: 'index', component: HomeComponent },
 ];
 
 @NgModule({
